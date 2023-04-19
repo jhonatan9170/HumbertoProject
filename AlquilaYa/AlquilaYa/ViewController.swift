198f9051805f86d44210fad86b93b4571d374173
Humberto Cabrera <111514994+frecabgue@users.noreply.github.com>
2023-04-19 15:57:20 -0700

    2do commit

--- AlquilaYa/AlquilaYa/ViewController.swift
+++ AlquilaYa/AlquilaYa/ViewController.swift
@@ -9,22 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let imagenes = ["alquiler.jpg","alquilerTemporal.jpg"]
+    
     @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 
         collectionView.dataSource = self
+      
+        collectionView.backgroundColor = .red
+        
+    //collectionView.delegate = self
+        collectionView.register(UINib(nibName: "myCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "mycell")
          }
 }
 
 extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
+        return imagenes.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "mycell", for: indexPath) as? myCollectionViewCell
+    
+        let imageName = imagenes [indexPath.row]
+        return cell!
     }
 }
 
---
+++ AlquilaYa/AlquilaYa/myCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  myCollectionViewCell.swift
+//  AlquilaYa
+//
+//  Created by Tismart on 19/04/23.
+//
+
+import UIKit
+
+class myCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var imagen: UIImageView!
+    @IBOutlet weak var etiqueta: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
