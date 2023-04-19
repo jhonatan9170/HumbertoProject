836f90df80715736f59c7feb24feea91cb462f24
Humberto Cabrera <111514994+frecabgue@users.noreply.github.com>
2023-04-19 11:52:57 -0700

    1er commit
    commit

---
+++ AlquilaYa/AlquilaYa/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  AlquilaYa
+//
+//  Created by Tismart on 19/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        collectionView.dataSource = self
+         }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+}
+
+
+
